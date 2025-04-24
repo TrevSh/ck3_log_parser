eb54c5507a35dc5d1df4f1bd4861f7cc0fc83be1
TrevSh <trevonshelby@outlook.com>
2025-04-24 10:20:15 -0500

    add exists check to get_default_windows_log_dir()

--- core/utils.py
+++ core/utils.py
@@ -3,4 +3,8 @@ from pathlib import Path
 def get_windows_default_log_dir():
     import os
     user = os.environ['USERPROFILE']
-    return Path(user) / 'Documents' / 'Paradox Interactive' / 'Crusader Kings III' / 'logs'
+    default_path = Path(user) / 'Documents' / 'Paradox Interactive' / 'Crusader Kings III' / 'logs'
+    if not default_path.exists():
+        # If the default path doesn't exist, return the current directory
+        return Path.cwd() / 'logs'
+    return
